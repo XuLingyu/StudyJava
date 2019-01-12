7ee1bb943705c4a9ffdfa825597713ff079d72e7
XuLingyu <lingyuxu@gmx.de>
2019-01-12 11:06:14 +0800

    delete

--- src/main/java/javaapplication/junit/TestCase1.java
+++ src/main/java/javaapplication/junit/TestCase1.java
@@ -8,25 +8,26 @@ import static org.junit.Assert.assertTrue;
 
 public class TestCase1 {
 
-	@Before
-	public void before() {
-		System.out.println("测试前的准备工作，比如链接数据库等等");
-	}
-	@After
-	public void after() {
-		System.out.println("测试结束后的工作，比如关闭链接等等");
-	}
-	
-	
+    @Before
+    public void before() {
+        System.out.println("测试前的准备工作，比如链接数据库等等");
+    }
+
+    @After
+    public void after() {
+        System.out.println("测试结束后的工作，比如关闭链接等等");
+    }
+
+
     @Test
     public void testSum1() {
-    	int result = SumUtil.sum1(1, 2);
-		assertTrue(result==3);
+        int result = SumUtil.sum1(1, 2);
+        assertTrue(result == 3);
     }
 
     @Test
     public void testSum2() {
-    	int result = SumUtil.sum2(1, 2,3);
-		assertTrue(result==6);
+        int result = SumUtil.sum2(1, 2, 3);
+        assertTrue(result == 5);
     }
 }
